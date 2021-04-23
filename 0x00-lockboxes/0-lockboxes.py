1d510b1703a87536a0c6b470eee6bca6a55cd25c
juangraram <juangraram@zohomail.com>
2021-04-22 21:59:31 -0500

    Change iterable

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-
+    # Box it's a list than tha key it's 0 forever Unlocked 
     listBoxUnlocked = [0]
-
+    
+    # 
     for keyUnlocked in listBoxUnlocked:
         for keyBoxes in boxes[keyUnlocked]:
-            if keyBoxes not in boxes[keyBoxes]:
+            if keyBoxes not in listBoxUnlocked:
                 if keyBoxes < len(boxes):
                     listBoxUnlocked.append(keyBoxes)
-    if len(listBoxUnlocked) == len(boxes):
-        return True
+                if len(listBoxUnlocked) == len(boxes):
+                    return True
     return False
