75f3ff2e6b24aa274fb6ab234cd7f642fef0f87e
juangraram <juangraram@zohomail.com>
2021-04-28 07:23:53 -0500

    Change the logic

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+"""
 def canUnlockAll(boxes):
     # Box it's a list than tha key it's 0 forever Unlocked 
     listBoxUnlocked = [0]
@@ -13,3 +14,30 @@ def canUnlockAll(boxes):
     if len(listBoxUnlocked) == len(boxes):
         return True
     return False
+""" 
+
+def canUnlockAll(boxes):
+
+    numbers = []
+    listunlock = []
+    len_boxes = len(boxes)
+
+    for keyboxes in boxes:
+        listunlock.append(0)
+    listunlock[0] = 1
+
+    for keyUnlocked in boxes[0]:
+        numbers.append(keyUnlocked)
+
+    for number in numbers:
+        if number < len_boxes and listunlock[number] == 0:
+            listunlock[number] = 1
+
+            for keyUnlocked in boxes[number]:
+                if keyUnlocked not in numbers:
+                    numbers.append(keyUnlocked)
+
+    if 0 in listunlock:
+        return False
+    else:
+        return True
