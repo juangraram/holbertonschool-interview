1e276768165f75218f3a1a5d8d81b5fa62a178b5
juangraram <juangraram@zohomail.com>
2021-06-06 11:28:57 -0500

    Change name arguments and make executable file

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,11 +4,11 @@ import sys
 import signal
 
 
-def log_parsing(function_size, status_code):
-    print('File size: {}'.format(function_size))
-    for key, value in sorted(status_code.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+def log_parsing(functionsize, statuscode):
+    print('File size: {}'.format(functionsize))
+    for keys, values in sorted(statuscode.items()):
+        if values != 0:
+            print('{}: {}'.format(keys, values))
 
 status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
                "403": 0, "404": 0, "405": 0, "500": 0}
