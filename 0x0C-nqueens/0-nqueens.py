90560c23adacc87af95b285de5eab5ad0f1c92e7
juangraram <juangraram@zohomail.com>
2021-07-26 07:40:33 -0500

    Some checks with errors

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,51 +1,57 @@
 #!/usr/bin/python3
-''' N queens algorithm '''
-import sys
+'''
+Module that solves the N Queens
+puzzle on an N×N chessboard
+'''
+from sys import argv, exit
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+    try:
+        n = int(argv[1])
+    except BaseException:
+        print('N must be a number')
+        exit(1)
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
 
-try:
-    n_q = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    exit(1)
+    solution = []
 
-if n_q < 4:
-    print("N must be at least 4")
-    exit(1)
+    def solve_queens(row, n, solution):
+        """Quenns placement in NxN chessboard
+        Args:
+            row (int): number of current row
+            n (int): number of rows
+            solution (array): Solution od N queens
+        """
+        if (row == n):
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_place(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
 
+    def valid_place(solution, placement):
+        """Valid Place of the current piece
+        Args:
+            solution (array): Solution od N queens
+            placement (array): Current piece placement
+        Returns:
+            [Bool]
+        """
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True
 
-def solve_nqueens(n):
-    '''self descriptive'''
-    if n == 0:
-        return [[]]
-    inner_solution = solve_nqueens(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(n_q)
-            for solution in inner_solution
-            if safe_queen((n, i + 1), solution)]
-
-
-def attack_queen(square, queen):
-    '''self descriptive'''
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
-
-
-def safe_queen(sqr, queens):
-    '''self descriptive'''
-    for queen in queens:
-        if attack_queen(sqr, queen):
-            return False
-    return True
-
-
-for answer in reversed(solve_nqueens(n_q)):
-    result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
-    print(result)
+    solve_queens(0, n, solution)
