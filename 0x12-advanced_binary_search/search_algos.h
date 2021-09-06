18f338758ae7c7bcecb50938fec73ee15143b0d4
juangraram <juangraram@zohomail.com>
2021-09-05 20:08:37 -0500

    Head

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef ADV_BINARY_SEARCH
+#define ADV_BINARY_SEARCH
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* ADV_BINARY_SEARCH */
